85b7651d0f67842b751060d747a50781cfd3a706
PRANTO <mdpranto417@gmail.com>
2024-11-27 14:26:15 +0600

    first commit

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+let num = 0;
+
+async function adviceGenerator() {
+    let apiUrl = `https://api.adviceslip.com/advice?timestamp=${new Date().getTime()}`;
+    let text = document.querySelector('.text');
+    let header = document.querySelector('.header');
+
+    try {
+        const response = await fetch(apiUrl);
+        let data = await response.json();
+        num++;
+
+       
+        let advice = `❝${data.slip.advice}❞`;
+        let chars = advice.split(''); 
+        text.textContent = ''; 
+        header.textContent = `Advice #${num}`;
+        let index = 0;
+
+        let interval = setInterval(() => {
+            if (index < chars.length) {
+                text.textContent += chars[index];
+                index++;
+            } else {
+                clearInterval(interval); 
+            }
+        }, 40); 
+    } catch (error) {
+        text.textContent = "❝Oops! Something went wrong.❞"; 
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', adviceGenerator);
